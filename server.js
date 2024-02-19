fd86925bed7ee10708b178f288e41246206fa332
raghav-oss <154777980+raghav-oss@users.noreply.github.com>
2024-02-19 15:40:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const config = {
-    user: 'Himanshu',
-    password: '130996',
-    server: 'SAP-3-156',
-    database: 'Orders_db',
+    user: 'sa',
+    password: 'sa@2014',
+    server: 'SERVERSAP',
+    database: 'INV1',
     options: {
         encrypt: false,
         trustServerCertificate: true, 
@@ -25,7 +25,7 @@ app.post('/login', async (req, res) => {
       const result = await pool.request()
         .input('username', sql.NVarChar, username)
         .input('password', sql.NVarChar, password)
-        .query('SELECT * FROM OrderData WHERE Name = @username AND Color = @password');
+        .query('SELECT * FROM OrderData WHERE DocEntry = @username AND BaseEntry = @password');
   
       if (result.recordset.length > 0) {
         res.json({ success: true });
